ee0ac2da253e34db7c8c197ef082cc7e120d6678
sergi <sergifusterdura@gmail.com>
2024-06-24 15:38:34 +0200

    Bug with tech filter fixed

--- portfolio/filters.py
+++ portfolio/filters.py
@@ -1,9 +1,8 @@
 from django import forms
-from .models import Project
+from .models import Project,Technology
 import django_filters
 
 class TechFilter(django_filters.FilterSet):
-    technology = django_filters.ModelMultipleChoiceFilter(queryset=Project.objects.all(), widget=forms.CheckboxSelectMultiple())
     class Meta:
         model = Project
         fields = ['technologies']
