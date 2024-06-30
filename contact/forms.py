34ec87c26b787134c3f6f53396921ca241e6c48d
sergi <sergifusterdura@gmail.com>
2024-06-30 12:01:05 +0200

    myself added on home, filter burger fixed, tbw added to portfolio, message on contact form, datetime added on post

--- contact/forms.py
+++ contact/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     company = forms.CharField(max_length=100, required=False)
-    email = forms.EmailField()
+    email = forms.EmailField(required=False)
     message = forms.CharField(widget=forms.Textarea(attrs={"rows": 5, "cols": 100}))
